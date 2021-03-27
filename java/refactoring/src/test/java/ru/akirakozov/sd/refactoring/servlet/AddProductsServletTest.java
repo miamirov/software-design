40a4c557c91a7ea046440bf0a1d905a3f48e8b72
miamirov <ildar.amirov@onetick.com>
2021-03-27 15:10:59 +0300

    add addProductsServlet tests

---
+++ java/refactoring/src/test/java/ru/akirakozov/sd/refactoring/servlet/AddProductsServletTest.java
@@ -0,0 +1,27 @@
+package ru.akirakozov.sd.refactoring.servlet;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.sql.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class addProductsServletTest extends AbstractServletTest {
+    protected addProductsServletTest() {
+        super(new AddProductServlet());
+    }
+
+    @Test
+    protected void testAdd() throws ServletException, IOException, SQLException {
+        dbInsertByRequest("1", 0);
+
+        servletService();
+
+        assertEquals("OK\n", getResponse());
+        assertEquals(getExpected(), dbSelectAll());
+    }
+
+}
